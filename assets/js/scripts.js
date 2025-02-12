473450bc03b3667b77872fc230ff5caf5bb684e0
bacdevv <vietbac.coding@gmail.com>
2025-02-12 21:53:22 +0700

    Filter Form #5

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -154,12 +154,12 @@ function initJsToggle() {
 		if (!target) {
 			document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
 		}
-		button.onclick = () => {
+		button.onclick = (e) => {
 			if (!$(target)) {
 				return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
 			}
 			const isHidden = $(target).classList.contains("hide");
-
+			e.preventDefault();
 			requestAnimationFrame(() => {
 				$(target).classList.toggle("hide", !isHidden);
 				$(target).classList.toggle("show", isHidden);
